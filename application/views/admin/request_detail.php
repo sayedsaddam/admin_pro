d5c484d9f3e61ac864bb93be0ec77585c46467d1
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2021-02-24 17:07:58 +0500

    Modified the request detail view file, added conditions on the action buttons.

--- application/views/admin/request_detail.php
+++ application/views/admin/request_detail.php
@@ -38,7 +38,16 @@
                 <!-- Card content -->
                 <div class="card-body font-weight-light">
                     <!-- Title -->
-                    <h4 class="card-title">Item Info</h4>
+                    <div class="row mb-0">
+                        <div class="col-lg-6 col-md-6">
+                            <h4 class="card-title">Item Info</h4>
+                        </div>
+                        <div class="col-lg-6 col-md-6 text-right">
+                            <h4 class="card-title font-weight-light text-info">
+                                <?php if($request_detail->status == 0){ echo "Pending..."; }elseif($request_detail->status == 1){ echo "Approved"; }else{ echo "Rejected"; } ?>
+                            </h4>
+                        </div>
+                    </div>
                     <hr>
                     <div class="row mb-4">
                         <div class="col-lg-3 col-md-3">
@@ -74,7 +83,7 @@
                     </div>
                     <div class="row mb-4">
                         <div class="col-lg-12 col-md-12">
-                            <a href="<?= base_url('admin/approve_request/'.$request_detail->id); ?>" class="btn btn-success"><i class="fa fa-check"></i> Approve</a>
+                            <a href="<?php if($request_detail->status == 0){ echo base_url('admin/approve_request/'.$request_detail->id); }else{ echo "#Approved"; } ?>" class="btn btn-success"><i class="fa fa-check"></i> Approve</a>
                             <a href="<?= base_url('admin/reject_request/'.$request_detail->id); ?>" class="btn btn-secondary"><i class="fa fa-times"></i> Reject</a>
                             <a href="javascript:history.go(-1)" class="btn btn-danger"><i class="fa fa-angle-left"></i> Back</a>
                         </div>
