448d741568c6cd530634af5e454691bc0f9d4e74
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2021-02-27 23:44:33 +0500

    Modified the head file for making the login page mobile friendly.

--- application/views/admin/commons/head.php
+++ application/views/admin/commons/head.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
   <!-- Google Fonts -->
