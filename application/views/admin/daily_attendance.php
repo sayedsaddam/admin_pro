6150d08552b0686dc662608b8eb02bb0e744d894
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2021-04-16 02:30:14 +0500

    Modified 2 view files, calculated time out based on approved time in for employees, and also calculated the early leave for specific employees.

--- application/views/admin/daily_attendance.php
+++ application/views/admin/daily_attendance.php
@@ -14,7 +14,7 @@
   </div>
 </div>
 
-<div class="container">
+<div class="container-fluid">
   <?php if($success = $this->session->flashdata('success')): ?>
     <div class="row">
       <div class="col-12">
@@ -41,14 +41,16 @@
     <div class="col-12">
       <div class="card card-list">
         <div class="card-header white d-flex justify-content-between align-items-center py-3 d-print-none">
-          <p class="h5-responsive font-weight-bold mb-0"><?php if(empty($results)){ echo 'Daily Attendance'; }else{ echo 'Search Results'; } ?> | <small class="d-print-none"><a href="javascript:history.go(-1)">&laquo; Back</a></small></p>
+          <p class="h5-responsive font-weight-bold mb-0"><?php if(empty($results)){ echo 'Daily Attendance Register'; }else{ echo 'Search Results - Daily Attendance Register'; } ?> | <small class="d-print-none"><a href="javascript:history.go(-1)">&laquo; Back</a></small></p>
           <ul class="list-unstyled d-flex align-items-center mb-0">
             <li><i class="far fa-window-minimize fa-sm pl-3"></i></li>
             <li><i class="fas fa-times fa-sm pl-3"></i></li>
           </ul>
         </div>
         <div class="card-body">
-          <h2 class="mb-4 d-none d-print-block">Attendance Report | <small class="font-weight-lighter">from <?= date('F jS', strtotime($_GET['date_from'])).' to '.date('F jS, Y', strtotime($_GET['date_to'])); ?></small></h2>
+          <?php if(!empty($results)): ?>
+            <h2 class="mb-4 d-none d-print-block">Attendance Report | <small class="font-weight-lighter">from <?= date('F jS', strtotime($_GET['date_from'])).' to '.date('F jS, Y', strtotime($_GET['date_to'])); ?></small></h2>
+          <?php else: echo "<h2 class='mb-4 d-none d-print-block'>Attendance Report</h2>"; endif; ?>
           <table class="table table-sm">
             <thead>
               <tr>
@@ -58,7 +60,8 @@
                 <th class="font-weight-bold">Check In</th>
                 <th class="font-weight-bold">Check Out</th>
                 <th class="font-weight-bold">Total Hours</th>
-                <th class="font-weight-bold">Late / Early</th>
+                <th class="font-weight-bold">Late Arrival</th>
+                <th class="font-weight-bold">Early Leave</th>
                 <th class="font-weight-bold">Date</th>
                 <th class="font-weight-bold d-print-none">Action</th>
               </tr>
@@ -72,15 +75,26 @@
                     // Late arrival calculations
                     $approved_time = date_create($att->approved_timings);
                     $arrival_time = date_create($att->time_in);
-                    $late_arrival = date_diff($approved_time, $arrival_time); ?>
+                    $late_arrival = date_diff($approved_time, $arrival_time); 
+                    $allowed_timings = $att->approved_timings;
+                    $leaving_time = '';
+                    if($allowed_timings == '09:00'){ $leaving_time .= '18:00'; }elseif($allowed_timings == '09:30'){ $leaving_time .= '18:30'; }elseif($allowed_timings == '10:00'){ $leaving_time .= '19:00'; }
+                    $approved_time_leave = date_create($leaving_time); // Approved time for leaving - taking off.
+                    $time_to_leave = date_create($att->time_out); // Time of employee leaving / taking off.
+                    $early_leave = date_diff($approved_time_leave, $time_to_leave); ?>
               <tr>
                 <td scope="row"><?= 'CTC-'.$att->id; ?></td>
                 <td><?= ucfirst($att->fullname); ?></td>
-                <td><?= $att->approved_timings; ?></td>
-                <td><?= date('h:i', strtotime($att->time_in)); ?></td>
-                <td><?= date('h:i', strtotime($att->time_out)); ?></td>
+                <td><?= date('h:ia', strtotime($att->approved_timings)).' - '.date('h:ia', strtotime($leaving_time)); ?></td>
+                <td><?= date('h:ia', strtotime($att->time_in)); ?></td>
+                <td><?= date('h:ia', strtotime($att->time_out)); ?></td>
                 <td><?= $diff->h.'h '.$diff->i.'m'; ?></td>
-                <td><?= $late_arrival->h.'h '.$late_arrival->i.'m'; ?></td>
+                <td>
+                  <?php if($att->time_in > $att->approved_timings){ echo $late_arrival->h.'h '.$late_arrival->i.'m'; }else{ echo 'In time.'; } ?>
+                </td>
+                <td>
+                  <?php if($att->time_out < $early_leave){ echo $early_leave->h.'h '.$early_leave->i.'m'; }else{ echo '--'; } ?>
+                </td>
                 <td><?= date('M d, Y', strtotime($att->created_at)); ?></td>
                 <td class="d-print-none">
                   <a href="#" class="badge badge-primary" title="Leave detail..."><i class="fa fa-print"></i></a>
@@ -93,27 +107,38 @@
               <tbody>
               <?php foreach($results as $res): ?>
               <?php $check_in = date_create($res->time_in);
-                    $check_out = date_create($res->time_out);
-                    $diff = date_diff($check_out, $check_in); 
-                    // Late arrival calculations
-                    $approved_time = date_create($res->approved_timings);
-                    $arrival_time = date_create($res->time_in);
-                    $late_arrival = date_diff($approved_time, $arrival_time); ?>
+                  $check_out = date_create($res->time_out);
+                  $diff = date_diff($check_out, $check_in); 
+                  // Late arrival calculations
+                  $approved_time = date_create($res->approved_timings);
+                  $arrival_time = date_create($res->time_in);
+                  $late_arrival = date_diff($approved_time, $arrival_time); 
+                  $allowed_timings = $res->approved_timings;
+                  $leaving_time = '';
+                  if($allowed_timings == '09:00'){ $leaving_time .= '18:00'; }elseif($allowed_timings == '09:30'){ $leaving_time .= '18:30'; }elseif($allowed_timings == '10:00'){ $leaving_time .= '19:00'; }
+                  $approved_time_leave = date_create($leaving_time); // Approved time for leaving - taking off.
+                  $time_to_leave = date_create($res->time_out); // Time of employee leaving / taking off.
+                  $early_leave = date_diff($approved_time_leave, $time_to_leave); ?>
               <tr>
                 <td scope="row"><?= 'CTC-'.$res->id; ?></td>
                 <td><?= ucfirst($res->fullname); ?></td>
-                <td><?= $res->approved_timings; ?></td>
-                <td><?= date('h:i', strtotime($res->time_in)); ?></td>
-                <td><?= date('h:i', strtotime($res->time_out)); ?></td>
+                <td><?= date('h:ia', strtotime($res->approved_timings)).' - '.date('h:ia', strtotime($leaving_time)); ?></td>
+                <td><?= date('h:ia', strtotime($res->time_in)); ?></td>
+                <td><?= date('h:ia', strtotime($res->time_out)); ?></td>
                 <td><?= $diff->h.'h '.$diff->i.'m'; ?></td>
-                <td><?= $late_arrival->h.'h '.$late_arrival->i.'m'; ?></td>
+                <td>
+                  <?php if($res->time_in > $res->approved_timings){ echo $late_arrival->h.'h '.$late_arrival->i.'m'; }else{ echo 'In time.'; } ?>
+                </td>
+                <td>
+                  <?php echo $early_leave->h.'h '.$early_leave->i.'m'; ?>
+                </td>
                 <td><?= date('M d, Y', strtotime($res->created_at)); ?></td>
                 <td class="d-print-none">
                   <a href="#" class="badge badge-primary" title="Leave detail..."><i class="fa fa-print"></i></a>
                   <a data-id="<?= $res->id; ?>" class="badge badge-danger reject_leave" title="Reject leave..."><i class="fa fa-times"></i></a>
                 </td>
               </tr>
-              <?php endforeach; echo '<tr class="d-print-none"><td colspan="8"><button class="btn btn-primary" onclick="javascript:window.print();">Print Report</button></td></tr>'; else: echo '<tr class="table-danger"><td colspan="9" align="center">No record found.</td></tr>'; ?>
+              <?php endforeach; echo '<tr class="d-print-none"><td colspan="10"><button class="btn btn-primary" onclick="javascript:window.print();">Print Report</button></td></tr>'; else: echo '<tr class="table-danger"><td colspan="9" align="center">No record found.</td></tr>'; ?>
             </tbody>
             <?php endif; ?>
           </table>
@@ -132,7 +157,7 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title w-100 font-weight-bold">Add Attendance</h4>
+        <h4 class="modal-title w-100 font-weight-bold">Add Attendance | <small class="font-weight-light">The time format to be used, must be 24hrs.</small></h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
--- application/views/admin/leaves.php
+++ application/views/admin/leaves.php
@@ -119,7 +119,7 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title w-100 font-weight-bold">Add Attendance</h4>
+        <h4 class="modal-title w-100 font-weight-bold">Add Attendance | <small class="font-weight-light">The time format to be used, must be 24hrs.</small></h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
