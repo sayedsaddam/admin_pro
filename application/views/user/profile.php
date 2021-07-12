4b1984a02c455a5c9e433bde473a94e1ee4db677
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2021-07-12 16:31:17 +0500

    Modified the profile view file, added a footer text to make the page scrollable. Also, adjusted the cards on profile page.

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -11,7 +11,7 @@
       <div class="col-lg-4 col-md-4 text-right">
         <button class="btn btn-outline-light font-weight-bold" title="Currently logged in..."><?php echo $this->session->userdata('fullname'); ?></button>
         <a href="<?= base_url('login/logout'); ?>" class="btn btn-dark font-weight-bold" title="Logout...">Logout <i class="fa fa-sign-out-alt"></i></a>
-        <h4 class="font-weight-bold orange-text mt-2">Profile <i class="fa fa-user"></i> <a href="<?= base_url('users'); ?>" class="text-light">Home</a></h4>
+        <h4 class="font-weight-bold orange-text mt-2">Employee Dashboard <i class="fa fa-chart-bar"></i><span class="font-weight-light"><br>User Profile</span> | <a href="<?= base_url('users'); ?>" class="text-light">Home</a></h4>
       </div>
     </div>
   </div>
@@ -81,40 +81,47 @@
         </div>
         <div class="col-md-7">
             <div class="card">
-            <h5 class="card-header morpheus-den-gradient white-text text-center py-4">
-                <strong>Update Profile</strong>
-            </h5>
+                <h5 class="card-header morpheus-den-gradient white-text text-center py-4">
+                    <strong>Update Profile</strong>
+                </h5>
                 <div class="card-body px-lg-5 pt-0">
-                    <form class="text-center" style="color: #757575;" action="<?= base_url('users/update_profile'); ?>" method="post">
+                    <form class="mt-2 mb-2" style="color: #757575;" action="<?= base_url('users/update_profile'); ?>" method="post">
                         <input type="hidden" name="user_id" value="<?= $profile->id; ?>">
                         <div class="form-row">
                             <div class="col">
                                 <div class="md-form">
-                                    <input name="fullname" type="text" id="materialRegisterFormFirstName" class="form-control">
+                                    <input name="fullname" type="text" id="materialRegisterFormFirstName" class="form-control" required>
                                     <label for="materialRegisterFormFirstName">Full Name</label>
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="md-form">
-                                    <input name="username" type="text" id="materialRegisterFormLastName" class="form-control">
+                                    <input name="username" type="text" id="materialRegisterFormLastName" class="form-control" required>
                                     <label for="materialRegisterFormLastName">Username</label>
                                 </div>
                             </div>
                         </div>
                         <div class="md-form mt-0">
-                            <input name="email" type="email" id="materialRegisterFormEmail" class="form-control">
+                            <input name="email" type="email" id="materialRegisterFormEmail" class="form-control" required>
                             <label for="materialRegisterFormEmail">Email</label>
                         </div>
-                        <div class="md-form">
-                            <input name="password" type="password" id="materialRegisterFormPassword" class="form-control">
+                        <div class="md-form mb-5">
+                            <input name="password" type="password" id="materialRegisterFormPassword" class="form-control" required>
                             <label for="materialRegisterFormPassword">Password</label>
                             <small class="deep-orange-text">Enter your existing password if you're not updating your password.</small>
                         </div>
-                        <button class="btn btn-outline-info my-4 waves-effect z-depth-0" type="submit">Update</button>
+                        <button class="btn btn-outline-success my-4 waves-effect z-depth-0" type="submit">Update</button>
                         <a href="javascript:history.go(-1)" class="btn btn-outline-orange my-4 waves-effect z-deprth-0">Back</a>
                     </form>
                 </div>
             </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-8"></div>
+        <div class="col-4 text-right mb-2">
+            <small>Copyright &copy; <?= '2019 - '.date('Y'); ?> <span class="purple-text">AH Group of Companies (Pvt.) Ltd.</span></small>
         </div>
     </div>
 </div>
