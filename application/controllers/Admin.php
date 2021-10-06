fcf457d0737146b201243090361970d54bb7d7bf
DawoodKMasood <91176669+DawoodKMasood@users.noreply.github.com>
2021-10-06 10:41:11 +0500

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1041,17 +1041,34 @@ class Admin extends CI_Controller{
     }
   //Available Item list
   public function available_item_list($offset = null){ 
-        $limit = 15;
-        if(!empty($offset)){
-        $this->uri->segment(3);
-        }
-    $url = 'admin/item_register';
+    $limit = 15;
+
+    if(!empty($offset)){
+        $config['uri_segment'] = 3;
+    }
+
+    $this->load->library('pagination');
+    $url = base_url('admin/item_register');
     $rowscount = $this->admin_model->count_item();
-    paginate($url, $rowscount, $limit);
+
+    $config['base_url'] = $url;
+    $config['total_rows'] = $rowscount;
+    $config['per_page'] = $limit;
+    $config['cur_tag_open'] = '<a class="pagination-link has-background-success has-text-white" aria-current="page">';
+    $config['cur_tag_close'] = '</a>';
+    $config['num_tag_open'] = '<li>';
+    $config['num_tag_open'] = '</li>';
+    $config['first_link'] = 'First';
+    $config['prev_link'] = 'Previous';
+    $config['next_link'] = 'Next';
+    $config['last_link'] = 'Last';
+    $config['attributes'] = array('class' => 'pagination-link');
+    $this->pagination->initialize($config);
+
     $data['title'] = 'Item Register | Admin & Procurement';
     $data['body'] = 'admin/item_assignment/item-register';
     $data['items'] = $this->admin_model->get_available_items($limit, $offset); 
-    $this->load->view('admin/commons/template', $data);
+    $this->load->view('admin/commons/new_template', $data);
 }
 //Assign item list
    public function get_assign_item($offset = null){  
