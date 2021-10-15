9dff56b8d4300b5a8ad848b6690191e487baec20
DawoodKMasood <91176669+DawoodKMasood@users.noreply.github.com>
2021-10-15 14:52:37 +0500

    Update foot.php

--- application/views/admin/commons/foot.php
+++ application/views/admin/commons/foot.php
@@ -1,10 +1,3 @@
-<!-- Bootstrap tooltips -->
-<script type="text/javascript" src="<?= base_url('assets/js/popper.min.js'); ?>"></script>
-<!-- Bootstrap core JavaScript -->
-<script type="text/javascript" src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>
-<!-- MDB core JavaScript -->
-<script type="text/javascript" src="<?= base_url('assets/js/mdb.min.js'); ?>"></script>
-
 </body>
 
 </html>
