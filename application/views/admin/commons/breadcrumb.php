3210bd3b65e90237d3fee82c723c7eaafb1644c1
DawoodKMasood <91176669+DawoodKMasood@users.noreply.github.com>
2021-10-19 19:22:31 +0500

    Update breadcrumb.php

--- application/views/admin/commons/breadcrumb.php
+++ application/views/admin/commons/breadcrumb.php
@@ -2,7 +2,8 @@
 <?php if ($this->uri->segment(2) == 'categories') : ?>
 <span class="has-text-black has-text-weight-bold">Categories List</span>
 <?php elseif ($this->uri->segment(2) == 'sub_categories' && !empty($parent_category)) : ?>
-    <a href="<?= base_url("admin/categories") ?>" class="has-text-black">Categories List</a> &raquo; <span class="has-text-weight-bold"><?= ucwords($parent_category[0]->cat_name) ?></span>
+<a href="<?= base_url("admin/categories") ?>" class="has-text-black">Categories List</a> &raquo; <span
+	class="has-text-weight-bold"><?= ucwords($parent_category[0]->cat_name) ?></span>
 <?php elseif ($this->uri->segment(2) == 'item_register') : ?>
 <span class="has-text-black has-text-weight-bold">Items List</span>
 <?php elseif ($this->uri->segment(2) == 'available_item_list') : ?>
@@ -18,13 +19,18 @@
 <?php elseif ($this->uri->segment(2) == 'product_report') : ?>
 <span class="has-text-black has-text-weight-bold">Report</span>
 <?php elseif ($this->uri->segment(2) == 'search_item') : ?>
-<span class="has-text-black has-text-weight-bold">Search</span>
+<a href="<?= base_url('admin/item_register') ?>" class="has-text-black">Items List</a> &raquo; <span
+	class="has-text-black has-text-weight-bold">Search</span>:
+<?= isset($_GET["search"]) ? filter_var($_GET["search"], FILTER_SANITIZE_STRING) : '' ?>
 <?php elseif ($this->uri->segment(2) == 'assign_item') : ?>
-    <a href="<?= base_url('admin/item_register') ?>" class="has-text-black">Items List</a> &raquo; <span class="has-text-black has-text-weight-bold">Assign Item (<?= ucwords($returning_items->names) ?>)</span>
+<a href="<?= base_url('admin/item_register') ?>" class="has-text-black">Items List</a> &raquo; <span
+	class="has-text-black has-text-weight-bold">Assign Item (<?= ucwords($returning_items->names) ?>)</span>
 <?php elseif ($this->uri->segment(2) == 'employ') : ?>
 <span class="has-text-black has-text-weight-bold">Employees List</span>
 <?php elseif ($this->uri->segment(2) == 'search_employ') : ?>
-<a href="<?= base_url('admin/employ') ?>" class="has-text-black">Employees List</a> &raquo; <span class="has-text-black has-text-weight-bold">Employees Search</span>: <?= isset($_GET["search"]) ? filter_var($_GET["search"], FILTER_SANITIZE_STRING) : '' ?>
+<a href="<?= base_url('admin/employ') ?>" class="has-text-black">Employees List</a> &raquo; <span
+	class="has-text-black has-text-weight-bold">Employees Search</span>:
+<?= isset($_GET["search"]) ? filter_var($_GET["search"], FILTER_SANITIZE_STRING) : '' ?>
 <?php elseif ($this->uri->segment(2) == 'suppliers') : ?>
 <span class="has-text-black has-text-weight-bold">Suppliers List</span>
 <?php endif ?>
