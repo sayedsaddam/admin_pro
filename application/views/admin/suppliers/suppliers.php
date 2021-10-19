f3ca3f71ec401da1af2dc43348bbfb0447df57a1
DawoodKMasood <91176669+DawoodKMasood@users.noreply.github.com>
2021-10-19 17:55:39 +0500

    Update suppliers.php

--- application/views/admin/suppliers/suppliers.php
+++ application/views/admin/suppliers/suppliers.php
@@ -1,12 +1,12 @@
 <?php $session = $this->session->userdata('user_role'); ?>
 <section class="columns is-gapless mb-0 pb-0">
-	<div class="column is-narrow is-fullheight is-hidden-print" style="background-color:#fafafa;">
+	<div class="column is-narrow is-fullheight is-hidden-print" id="custom-sidebar">
 		<?php $this->view('admin/commons/sidebar'); ?>
 	</div>
 	<div class="column">
 		<div class="columns">
 			<div class="column section">
-      <div class="columns">
+      			<div class="columns">
 					<div class="column">
 						<?php $this->view('admin/commons/breadcrumb'); ?>
 					</div>
