6c71a114ec8139dac9f1b7e57beda4e20bc587d7
DawoodKMasood <91176669+DawoodKMasood@users.noreply.github.com>
2021-10-19 17:40:46 +0500

    removed hard-code style from sidebar element and added it to `custom.css` file

--- application/views/admin/categories.php
+++ application/views/admin/categories.php
@@ -1,5 +1,5 @@
  <section class="columns is-gapless mb-0 pb-0">
- 	<div class="column is-narrow is-fullheight is-hidden-print" style="background-color:#fafafa;">
+ 	<div class="column is-narrow is-fullheight is-hidden-print" id="custom-sidebar">
  		<?php $this->view('admin/commons/sidebar'); ?>
  	</div>
  	<div class="column">
