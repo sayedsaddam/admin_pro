82b144fcc347673e22af402447af94605dc3106b
DawoodKMasood <91176669+DawoodKMasood@users.noreply.github.com>
2021-10-20 14:34:30 +0500

    Update breadcrumb.php

--- application/views/admin/commons/breadcrumb.php
+++ application/views/admin/commons/breadcrumb.php
@@ -4,7 +4,7 @@
         <?php if ($index === array_key_last($breadcrumb)) : ?>
             &raquo; <span class="has-text-black has-text-weight-bold"><?= ucwords($value) ?></span>
         <?php else: ?>
-            &raquo; <a href="<?= base_url($index) ?>" class="has-text-black"><?= ucwords($value) ?></a>
+            &raquo; <a href="<?= isset($index) ? base_url($index) : '#' ?>" class="has-text-black"><?= ucwords($value) ?></a>
         <?php endif ?>
     <?php endforeach ?>
 <?php endif ?>
