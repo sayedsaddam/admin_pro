1bed6575a28ff8d99753423e8d7f96b2c11007a5
DawoodKMasood <91176669+DawoodKMasood@users.noreply.github.com>
2021-10-20 15:43:05 +0500

    Applied old changes

--- application/views/admin/commons/breadcrumb.php
+++ application/views/admin/commons/breadcrumb.php
@@ -1,10 +1,10 @@
 <a href="<?= base_url('admin') ?>" class="has-text-black">Admin</a> 
 <?php if(isset($breadcrumb)) : ?>
     <?php foreach($breadcrumb as $index => $value ) : ?>
-        <?php if ($index === array_key_first($breadcrumb) && is_string($index)) : ?>
-        &raquo; <a href="<?= base_url($index) ?>" class="has-text-black"><?= ucwords($value) ?></a>
-        <?php elseif ($index === array_key_last($breadcrumb)) : ?>
-        &raquo; <span class="has-text-black has-text-weight-bold"><?= ucwords($value) ?></span>
+        <?php if ($index === array_key_last($breadcrumb)) : ?>
+            &raquo; <span class="has-text-black has-text-weight-bold"><?= ucwords($value) ?></span>
+        <?php else: ?>
+            &raquo; <a href="<?= isset($index) ? base_url($index) : '#' ?>" class="has-text-black"><?= ucwords($value) ?></a>
         <?php endif ?>
     <?php endforeach ?>
 <?php endif ?>
