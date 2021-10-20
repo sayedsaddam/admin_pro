e4bc48bc246583cd0c7fb6a331251319a59c91dc
DawoodKMasood <91176669+DawoodKMasood@users.noreply.github.com>
2021-10-20 12:08:14 +0500

    moved the `Report` button to the right side of container

--- application/views/admin/item_assignment/item-detail.php
+++ application/views/admin/item_assignment/item-detail.php
@@ -32,15 +32,6 @@
 					<div class="column is-hidden-touch">
 						<div class="field has-addons">
 							<p class="control">
-								<button class="button is-small <?= isset($product_report) ? 'has-background-primary-light' : '' ?>"
-									id="report-btn">
-									<span class="icon is-small">
-										<i class="fas fa-paperclip"></i>
-									</span>
-									<span>Report</span>
-								</button>
-							</p>
-							<p class="control">
 								<button onclick="location.href='<?= base_url('admin/item_register'); ?>'"
 									class="button is-small <?= isset($item_register) ? 'has-background-primary-light' : '' ?>">
 									<span class="icon is-small">
@@ -74,6 +65,16 @@
 										<i class="fas fa-plus"></i>
 									</span>
 									<span>Add New</span>
+								</button>
+							</p>
+							<p class="control">
+								<button
+									class="button is-small <?= (isset($product_report)) ? 'has-background-primary-light' : '' ?>"
+									id="report-btn">
+									<span class="icon is-small">
+										<i class="fas fa-paperclip"></i>
+									</span>
+									<span>Report</span>
 								</button>
 							</p>
 						</div>
