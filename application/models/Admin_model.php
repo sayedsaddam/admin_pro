b74bb6e6e1998dd9fbcdb2937c336a917f9e208b
DawoodKMasood <dawood.masood@s2smark.com>
2021-10-25 18:05:24 +0500

    Update Admin_model.php

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -1368,11 +1368,6 @@ class Admin_model extends CI_Model{
     public function get_item_depreciation($id){
         return $this->db->from('items')->where('id',$id)->get()->result();
     }
-    // get items status for edit item
-    public function status_items($id){
-        $this->db->select('id,status');
-        return $this->db->from('items')->where('id',$id)->get()->result();
-    }
     // Get sub categories based on cat_id
     public function get_item_sub_categories($cat_id){
         $this->db->select('id, name');
