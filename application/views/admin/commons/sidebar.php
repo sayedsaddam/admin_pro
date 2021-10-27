7fc4c6b9ff77066abdd3161504fd156e00712425
DawoodKMasood <dawood.masood@s2smark.com>
2021-10-27 17:40:15 +0500

    Update sidebar.php

--- application/views/admin/commons/sidebar.php
+++ application/views/admin/commons/sidebar.php
@@ -52,6 +52,7 @@
 		</li>
 	</ul>
 	<?php endif ?>
+	<?php if($this->session->userdata('user_role') == 'admin' || $this->session->userdata('user_role') != 'user') : ?>
 	<ul class="menu-list">
 		<li><a class="<?= $this->uri->segment(2) == 'categories' || $this->uri->segment(2) == 'sub_categories' || $this->uri->segment(2) == 'search_sub_categories' || $this->uri->segment(2) == 'search_categories' ? 'is-primary has-text-weight-bold is-inverted' : '' ?> nav-category"
 				<?= $this->uri->segment(2) == 'categories' || $this->uri->segment(2) == 'sub_categories' || $this->uri->segment(2) == 'search_sub_categories' || $this->uri->segment(2) == 'search_categories' ? 'style="background-color:#ebfffc;"' : '' ?>>Item
@@ -63,6 +64,7 @@
 			</ul>
 		</li>
 	</ul>
+	<?php endif ?>
 	<ul class="menu-list">
 		<!-- <li><a href="<?= base_url('admin/travels_info') ?>">Travels Info</a></li> -->
 	</ul>
