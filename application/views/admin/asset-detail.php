03a977d2425708177e411f60ac65e11cbb278aa0
DawoodKMasood <dawood.masood@s2smark.com>
2021-11-12 17:16:30 +0500

    Revert "Update asset-detail.php"
    This reverts commit 847f1d58b918b003c41219d5b5dd86bc0ed28f6c.

--- application/views/admin/asset-detail.php
+++ application/views/admin/asset-detail.php
@@ -56,200 +56,186 @@
 						</div>
 					</div>
 				</div>
+
+				<?php if($this->session->flashdata('success')) : ?>
+				<div class="columns">
+					<div class="column">
+						<div class="notification is-success is-light">
+							<button class="delete"></button>
+							<?= $message = $this->session->flashdata('success'); ?>
+						</div>
+					</div>
+				</div>
+				<?php elseif($this->session->flashdata('failed')) : ?>
+				<div class="columns">
+					<div class="column">
+						<div class="notification is-danger is-light">
+							<button class="delete"></button>
+							<?= $message = $this->session->flashdata('failed'); ?>
+						</div>
+					</div>
+				</div>
+				<?php endif ?>
+
 				<form
 					action="<?php if(empty($edit)){ echo base_url('admin/save_item'); }else{ echo base_url('admin/update_item'); } ?>"
 					method="post">
-					<div class="tile is-ancestor">
-						<div class="tile is-parent">
-							<div class="tile is-child box">
-								<?php if($this->session->flashdata('success')) : ?>
-								<div class="columns">
-									<div class="column">
-										<div class="notification is-success is-light">
-											<button class="delete"></button>
-											<?= $message = $this->session->flashdata('success'); ?>
-										</div>
+					<input type="hidden" name="id" value="<?php echo $this->uri->segment(3); ?>">
+					<div class="columns">
+						
+					<div class="column">
+						<fieldset>
+								<div class="field">
+									<label class="label is-small">Category <span class="has-text-danger">*</span></label>
+									<div class="control has-icons-left">
+										<span class="select is-small is-fullwidth">
+											<select name="category" id="category" required>
+												<option selected disabled value="">Select a Category</option>
+												<?php if(!empty($categories)): foreach($categories as $cat): ?>
+												<option value="<?= $cat->id; ?>"
+													<?= !empty($edit) && $edit->category == $cat->id ? 'selected' : '' ?>>
+													<?= ucwords($cat->cat_name); ?>
+												</option>
+												<?php endforeach; endif; ?>
+											</select>  
+										</span>
+										<span class="icon is-small is-left">
+											<i class="fas fa-tags"></i>
+										</span>
 									</div>
 								</div>
-								<?php elseif($this->session->flashdata('failed')) : ?>
-								<div class="columns">
-									<div class="column">
-										<div class="notification is-danger is-light">
-											<button class="delete"></button>
-											<?= $message = $this->session->flashdata('failed'); ?>
-										</div>
+							</fieldset> 
+						</div>
+
+						<div class="column">
+							<fieldset>
+								<div class="field">
+									<label class="label is-small">Subcategory <span class="has-text-danger">*</span></label>
+									<div class="control has-icons-left">
+										<span class="select is-small is-fullwidth">
+											 
+											<select name="sub_categories" id="sub_categories" required>
+												<option selected disabled value="">Select a Sub Categories</option>
+												<?php if(!empty($sub_categories)): foreach($sub_categories as $cat): ?>
+												<option value="<?= $cat->id; ?>"
+													<?= !empty($edit) && $edit->sub_categories == $cat->id ? 'selected' : '' ?>>
+													<?= ucwords($cat->name); ?>
+												</option>
+												<?php endforeach; endif; ?>
+											</select>  
+										</span>
+										<span class="icon is-small is-left">
+											<i class="fas fa-luggage-cart"></i>
+										</span>
 									</div>
 								</div>
-								<?php endif ?>
-								<input type="hidden" name="id" value="<?php echo $this->uri->segment(3); ?>">
-								<div class="columns">
+							</fieldset>
+						</div>
 
-									<div class="column">
-										<fieldset>
-											<div class="field">
-												<label class="label is-small">Category <span
-														class="has-text-danger">*</span></label>
-												<div class="control has-icons-left">
-													<span class="select is-small is-fullwidth">
-														<select name="category" id="category" required>
-															<option selected disabled value="">Select a Category
-															</option>
-															<?php if(!empty($categories)): foreach($categories as $cat): ?>
-															<option value="<?= $cat->id; ?>"
-																<?= !empty($edit) && $edit->category == $cat->id ? 'selected' : '' ?>>
-																<?= ucwords($cat->cat_name); ?>
-															</option>
-															<?php endforeach; endif; ?>
-														</select>
-													</span>
-													<span class="icon is-small is-left">
-														<i class="fas fa-tags"></i>
-													</span>
-												</div>
-											</div>
-										</fieldset>
-									</div>
+					</div>
 
-									<div class="column">
-										<fieldset>
-											<div class="field">
-												<label class="label is-small">Subcategory <span
-														class="has-text-danger">*</span></label>
-												<div class="control has-icons-left">
-													<span class="select is-small is-fullwidth">
-
-														<select name="sub_categories" id="sub_categories" required>
-															<option selected disabled value="">Select a Sub Categories
-															</option>
-															<?php if(!empty($sub_categories)): foreach($sub_categories as $cat): ?>
-															<option value="<?= $cat->id; ?>"
-																<?= !empty($edit) && $edit->sub_categories == $cat->id ? 'selected' : '' ?>>
-																<?= ucwords($cat->name); ?>
-															</option>
-															<?php endforeach; endif; ?>
-														</select>
-													</span>
-													<span class="icon is-small is-left">
-														<i class="fas fa-luggage-cart"></i>
-													</span>
-												</div>
-											</div>
-										</fieldset>
-									</div>
-
-								</div>
-
-								<div class="columns">
-									<div class="column">
-										<div class="control">
-											<label class="label is-small">Quantity <span
-													class="has-text-danger">*</span></label>
-											<div class="select is-small is-fullwidth">
-												<div class="control has-icons-left">
-													<input type="number" name="quantity" id="" class="input is-small"
-														value="<?php if(!empty($edit)){ echo $edit->quantity; } ?>"
-														type="text" placeholder="1-99" required="">
-													<span class="icon is-small is-left">
-														<i class="fas fa-sort-numeric-up"></i>
-													</span>
-												</div>
-											</div>
-										</div>
-									</div>
-
-									<div class="column">
-										<div class="control">
-											<label class="label is-small">Price (PKR) <span
-													class="has-text-danger">*</span></label>
-											<div class="select is-small is-fullwidth">
-												<div class="control has-icons-left">
-													<input type="number" name="price" id="" class="input is-small"
-														value="<?php if(!empty($edit)){ echo $edit->price; } ?>"
-														type="text" placeholder="1-9,999,999" required="">
-													<span class="icon is-small is-left">
-														<i class="far fa-money-bill-alt"></i>
-													</span>
-												</div>
-											</div>
-										</div>
+					<div class="columns"> 
+						<div class="column">
+							<div class="control">
+								<label class="label is-small">Quantity <span class="has-text-danger">*</span></label>
+								<div class="select is-small is-fullwidth">
+									<div class="control has-icons-left">
+										<input type="number" name="quantity" id="" class="input is-small"
+											value="<?php if(!empty($edit)){ echo $edit->quantity; } ?>" type="text"
+											placeholder="1-99" required="">
+										<span class="icon is-small is-left">
+											<i class="fas fa-sort-numeric-up"></i>
+										</span>
 									</div>
 								</div>
-
-
-								<div class="columns">
-									<div class="column">
-										<div class="control">
-											<label class="label is-small">Purchase Date <span
-													class="has-text-danger">*</span></label>
-											<div class="is-small is-fullwidth">
-												<div class="control has-icons-left">
-													<?php if(!empty($edit)){$date = strtotime($edit->purchase_date); $purchase_date = date('Y-m-d', $date); }?>
-													<input name="purchase_date" class="input is-small" type="date"
-														required
-														value="<?php if(!empty($edit)){ echo $purchase_date; } ?>">
-													<span class="icon is-small is-left">
-														<i class="far fa-calendar-alt"></i>
-													</span>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="column">
-										<div class="control">
-											<label class="label is-small">Location <span
-													class="has-text-danger">*</span></label>
-											<div class="control has-icons-left">
-												<span class="select is-small is-fullwidth">
-													<select name="location" id=""
-														class="browser-default custom-select ">
-														<option disabled value="">Select Category</option>
-														<?php if(!empty($locations)): foreach($locations as $loc): ?>
-														<?php if ($loc->id == $this->session->userdata('location') || $this->session->userdata('user_role') == '1') : ?>
-														<option value="<?= $loc->id; ?>"
-															<?= !empty($edit) && $edit->location == $loc->id ? 'selected' : '' ?>>
-															<?= ucwords($loc->name); ?>
-														</option>
-														<?php endif ?>
-														<?php endforeach; endif; ?>
-													</select>
-												</span>
-												<span class="icon is-small is-left">
-													<i class="fas fa-globe"></i>
-												</span>
-											</div>
-										</div>
+							</div>
+						</div> 
+						
+						<div class="column">
+							<div class="control">
+								<label class="label is-small">Price (PKR) <span class="has-text-danger">*</span></label>
+								<div class="select is-small is-fullwidth">
+									<div class="control has-icons-left">
+										<input type="number" name="price" id="" class="input is-small"
+											value="<?php if(!empty($edit)){ echo $edit->price; } ?>" type="text"
+											placeholder="1-9,999,999" required="">
+										<span class="icon is-small is-left">
+											<i class="far fa-money-bill-alt"></i>
+										</span>
 									</div>
 								</div>
-								<div class="columns">
-									<div class="column">
-										<fieldset>
-											<div class="field">
-												<label class="label is-small">Description</label>
-												<div class="control has-icons-left">
-													<textarea class="textarea is-small" name="description" rows="2"
-														id=""
-														placeholder="Enter brief description of the asset you are adding."><?php if(!empty($edit)){ echo $edit->description; } ?></textarea>
+							</div>
+						</div> 
+					</div>
 
-												</div>
-											</div>
-										</fieldset>
+
+					<div class="columns">
+						<div class="column">
+							<div class="control">
+								<label class="label is-small">Purchase Date <span
+										class="has-text-danger">*</span></label>
+								<div class="is-small is-fullwidth">
+									<div class="control has-icons-left">
+										<?php if(!empty($edit)){
+            $date = strtotime($edit->purchase_date); 
+            $purchase_date = date('Y-m-d', $date); 
+            }?>
+										<input name="purchase_date" class="input is-small" type="date" required
+											value="<?php if(!empty($edit)){ echo $purchase_date; } ?>">
+										<span class="icon is-small is-left">
+											<i class="far fa-calendar-alt"></i>
+										</span>
 									</div>
 								</div>
-
-
+							</div>
+						</div>
+						<div class="column">
+							<div class="control">
+								<label class="label is-small">Location <span class="has-text-danger">*</span></label>
+								<div class="control has-icons-left">
+									<span class="select is-small is-fullwidth">
+										<select name="location" id="" class="browser-default custom-select ">
+											<option disabled value="">Select Category</option>
+											<?php if(!empty($locations)): foreach($locations as $loc): ?>
+											<?php if ($loc->id == $this->session->userdata('location') || $this->session->userdata('user_role') == '1') : ?>
+											<option value="<?= $loc->id; ?>"
+												<?= !empty($edit) && $edit->location == $loc->id ? 'selected' : '' ?>>												
+												<?= ucwords($loc->name); ?>
+											</option>
+											<?php endif ?>
+											<?php endforeach; endif; ?>
+										</select>
+									</span>
+									<span class="icon is-small is-left">
+										<i class="fas fa-globe"></i>
+									</span>
+								</div>
 							</div>
 						</div>
 					</div>
 					<div class="columns">
+						<div class="column">
+							<fieldset>
+								<div class="field">
+									<label class="label is-small">Description</label>
+									<div class="control has-icons-left">
+										<textarea class="textarea is-small" name="description" rows="2" id=""
+											placeholder="Enter brief description of the asset you are adding."><?php if(!empty($edit)){ echo $edit->description; } ?></textarea>
+
+									</div>
+								</div>
+							</fieldset>
+						</div>
+					</div>
+
+					<div class="columns">
 						<div class="column has-text-right">
 							<div class="buttons is-pulled-right">
 								<?php if(!isset($edit_item)): ?>
-								<button class="button is-danger is-small is-outlined" type="reset">Reset
-									Form</button>
+								<button class="button is-danger is-small is-outlined" type="reset">Reset Form</button>
 								<?php endif ?>
 								<p class="control">
-									<button class="button is-small is-success" type="submit"
-										<?= isset($edit) && $AssetsAccess->update == 0 || $AssetsAccess->write == 0 ? 'disabled' : '' ?>>
+									<button class="button is-small is-success" type="submit" <?= isset($edit) && $AssetsAccess->update == 0 || $AssetsAccess->write == 0 ? 'disabled' : '' ?>>
 										<span><?= !isset($edit_item) ? 'Save and continue' : 'Save Changes' ?></span>
 										<span class="icon is-small">
 											<i class="fas fa-arrow-right"></i>
@@ -259,38 +245,37 @@
 							</div>
 						</div>
 					</div>
-
 				</form>
 			</div>
 		</div>
 	</div>
 </section>
 <script>
-	$(document).ready(function () {
+$(document).ready(function () {
 
-		// category change
-		$('#category').on('change', function () {
-			// AJAX request
-			var category = $(this).val();
-			$.ajax({
-				url: '<?= base_url("admin/get_item_sub_categories/"); ?>' + category,
-				method: 'POST',
-				data: {
-					category: category
-				},
-				dataType: 'json',
-				success: function (response) {
-					// Remove options 
-					$('#sub_categories').find('option').not(':first').remove();
+// category change
+$('#category').on('change', function () {
+	// AJAX request
+	var category = $(this).val();
+	$.ajax({
+		url: '<?= base_url("admin/get_item_sub_categories/"); ?>' + category,
+		method: 'POST',
+		data: {
+			category: category
+		},
+		dataType: 'json',
+		success: function (response) {
+			// Remove options 
+			$('#sub_categories').find('option').not(':first').remove();
 
-					// Add options
-					$.each(response, function (index, data) {
-						$('#sub_categories').append('<option value="' + data['id'] +
-							'">' + data['name'] + '</option>');
-					});
-				}
+			// Add options
+			$.each(response, function (index, data) {
+				$('#sub_categories').append('<option value="' + data['id'] + '">' + data['name'] + '</option>');
 			});
-		});
+		}
 	});
+});
+});
+
 
 </script>
