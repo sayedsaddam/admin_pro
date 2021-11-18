65fbe836cbc2afac3abdb9dd097e1a1eab014bda
DawoodKMasood <dawood.masood@s2smark.com>
2021-11-18 10:40:47 +0500

    Update add_supplier.php

--- application/views/admin/suppliers/add_supplier.php
+++ application/views/admin/suppliers/add_supplier.php
@@ -28,6 +28,14 @@
 										</span>
 									</button>
 								</div>
+								<div class="ml-1 control">
+									<a href="<?= base_url('report/supplier_report') ?>" class="button is-small">
+										<span class="icon is-small">
+											<i class="fas fa-sort-alpha-down"></i>
+										</span>
+										<span>Filter</span>
+									</a>
+								</div>
 							</div>
 						</form>
 					</div>
