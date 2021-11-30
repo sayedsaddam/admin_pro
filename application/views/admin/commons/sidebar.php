0642a6b2db49689c0b28df1327bbdf28eedb05cf
AvalerionHF <dawood.masood@s2smark.com>
2021-11-30 11:06:17 +0500

    Update sidebar.php

--- application/views/admin/commons/sidebar.php
+++ application/views/admin/commons/sidebar.php
@@ -15,6 +15,26 @@
 		</li>
 	</ul>
 	<p class="menu-label">
+		Requisition
+	</p>
+	<ul class="menu-list">
+		<li><a class="<?= $this->uri->segment(2) == 'get_assign_item' ? 'is-primary is-inverted' : '' ?> nav-category"
+				<?= $this->uri->segment(2) == 'get_assign_item' ? 'style="background-color:#f1f1f1;"' : '' ?>>Item
+				Requisition</a>
+			<ul class="sub-categories"
+				style="<?= $this->uri->segment(2) == 'get_assign_item' ? '' : 'display: none;' ?>">
+				<li
+					class="is-size-7 <?= $this->uri->segment(2) == 'item_register' ? 'has-text-weight-bold' : '' ?>">
+					<a href="<?= base_url('admin/add_requisition'); ?>"><i class="fas fa-plus mr-1"></i>
+						Add Request</a></li>
+				<li
+					class="is-size-7 <?= $this->uri->segment(2) == 'available_item_list' ? 'has-text-weight-bold' : '' ?>">
+					<a href="<?= base_url('admin/requisition_list'); ?>"><i class="fas fa-list mr-1"></i>
+						Request List</a></li>
+			</ul>
+		</li>
+	</ul>
+	<p class="menu-label">
 		Procurement
 	</p>
 	<?php if($AssetsAccess->read == 1) : ?>
@@ -87,23 +107,6 @@
 		</li>
 	</ul>
 	<?php endif ?>
-	<ul class="menu-list">
-		<li><a class="<?= $this->uri->segment(2) == 'get_assign_item' ? 'is-primary is-inverted' : '' ?> nav-category"
-				<?= $this->uri->segment(2) == 'get_assign_item' ? 'style="background-color:#f1f1f1;"' : '' ?>>Item
-				Requisition</a>
-			<ul class="sub-categories"
-				style="<?= $this->uri->segment(2) == 'get_assign_item' ? '' : 'display: none;' ?>">
-				<li
-					class="is-size-7 <?= $this->uri->segment(2) == 'item_register' ? 'has-text-weight-bold' : '' ?>">
-					<a href="<?= base_url('admin/add_requisition'); ?>"><i class="fas fa-plus mr-1"></i>
-						Add Request</a></li>
-				<li
-					class="is-size-7 <?= $this->uri->segment(2) == 'available_item_list' ? 'has-text-weight-bold' : '' ?>">
-					<a href="<?= base_url('admin/requisition_list'); ?>"><i class="fas fa-list mr-1"></i>
-						Request List</a></li>
-			</ul>
-		</li>
-	</ul>
 	<?php if($RegisterAccess->read == 1) : ?>
 	<ul class="menu-list">
 		<li><a class="<?= $this->uri->segment(2) == 'get_assign_item' || $this->uri->segment(2) == 'available_item_list' || $this->uri->segment(2) == 'item_register' || $this->uri->segment(2) == 'product_report' || $this->uri->segment(2) == 'add_item' || $this->uri->segment(2) == 'search_item' || $this->uri->segment(2) == 'item_detail' || $this->uri->segment(2) == 'get_damaged_item'  || $this->uri->segment(2) == 'assign_item' ? 'is-primary is-inverted' : '' ?> nav-category"
