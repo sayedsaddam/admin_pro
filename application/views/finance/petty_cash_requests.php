78921a0be9f3ee2ee28b88dc99146b40eecfda67
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2021-12-30 00:09:29 +0500

    Modified petty cash requests, blocked access for finance executive to admin dashboard.

--- application/views/finance/petty_cash_requests.php
+++ application/views/finance/petty_cash_requests.php
@@ -8,7 +8,7 @@
       <div class="col-lg-4 col-md-4 text-right">
         <button class="btn btn-outline-light font-weight-bold" title="Currently logged in..."><?php echo $this->session->userdata('fullname'); ?></button>
         <a href="<?= base_url('login/logout'); ?>" class="btn btn-dark font-weight-bold" title="Logout...">Logout <i class="fa fa-sign-out-alt"></i></a>
-        <h4 class="font-weight-bold orange-text mt-2">Finance Dashboard <i class="fa fa-chart-bar"></i><br><span class="font-weight-light">Petty Cash Requests</span> | <a href="<?=base_url('admin');?>" class="text-light">Home</a></h4>
+        <h4 class="font-weight-bold orange-text mt-2">Finance Dashboard <i class="fa fa-chart-bar"></i><br><span class="font-weight-light">Petty Cash Requests</span> | <a href="<?=base_url('users');?>" class="text-light">Home</a></h4>
       </div>
     </div>
   </div>
